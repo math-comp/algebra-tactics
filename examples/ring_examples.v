28a1baa21dcdb089bef3f76f5f257c5d512b89a0
Kazuhiko Sakaguchi <pi8027@gmail.com>
2021-10-01 17:30:18 +0900

    Reorganize ring_examples.v

--- examples/ring_examples.v
+++ examples/ring_examples.v
@@ -19,28 +19,49 @@ Variables (R : comRingType) (a b c : R).
 
 
 (* Using the _%:R embedding from nat to R *)
-Goal 
-    (a + b + c) ^+ 2 =
-    a * a + b ^+ 2 + c * c + 2%:R * a * b + 2%:R * a * c + 2%:R * b * c.
+Goal (a + b + c) ^+ 2 =
+     a * a + b ^+ 2 + c * c + 2%:R * a * b + 2%:R * a * c + 2%:R * b * c.
 Proof. ring. Qed.
 
 (* Using the _%:~R embedding from int to R : 2 is coerced to (Posz 2) : int *)
-Goal 
-    (a + b + c) ^+ 2 =
-    a * a + b ^+ 2 + c * c + 2%:~R * a * b + 2%:~R * a * c + 2%:~R * b * c.
+Goal (a + b + c) ^+ 2 =
+     a * a + b ^+ 2 + c * c + 2%:~R * a * b + 2%:~R * a * c + 2%:~R * b * c.
 Proof. ring. Qed.
 
 (* With an identity hypothesis *)
 (* Using the _%:R embedding from nat to R *)
-Goal 
-    2%:R * a * b = 30%:R -> (a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 30%:R.
+Goal 2%:R * a * b = 30%:R -> (a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 30%:R.
 Proof. move=> H; ring: H. Qed.
 
 (* With an identity hypothesis *)
 (* Using the _%:~R embedding from int to R *)
-Goal 
-    2%:~R * a * b = 30%:~R -> (a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 30%:~R.
+Goal 2%:~R * a * b = 30%:~R -> (a + b) ^+ 2 = a ^+ 2 + b ^+ 2 + 30%:~R.
 Proof. move=> H; ring: H. Qed.
+
+End AbstractCommutativeRing.
+
+Section AbstractRingMorphism.
+
+Variables (R : ringType) (R' : comRingType) (f : {rmorphism R -> R'}) (a b : R).
+
+Goal f ((a + b) ^+ 2) = f a ^+ 2 + f b ^+ 2 + 2%:R * f a * f b.
+Proof. ring. Qed.
+
+End AbstractRingMorphism.
+
+Section AbstractAdditiveFunction.
+
+Variables (V' V : zmodType) (R : comRingType).
+Variables (g : {additive V' -> V}) (f : {additive V -> R}) (a : V') (b : V).
+
+Goal f (g a + b) ^+ 2 = f (g a) ^+ 2 + f b ^+ 2 + f (g (a *+ 2)) * f b.
+Proof. ring. Qed.
+
+End AbstractAdditiveFunction.
+
+Section NumeralExamples.
+
+Variable (R : comRingType).
 
 (* With numeral constants *)
 Goal 20%:R * 3%:R = 60%:R :> R.
@@ -52,49 +73,25 @@ Proof. ring. Qed.
 Goal 200%:~R * 30%:~R = 6000%:~R :> R.
 Proof. ring. Qed.
 
-
 Goal 2%:~R * 10%:~R ^+ 2 * 3%:~R * 10%:~R ^+ 2 = 6%:~R * 10%:~R ^+ 4:> R.
 Proof. ring. Qed.
 
-End AbstractCommutativeRing.
-
-Section RingMorphism.
-
-Variables (R : ringType) (R' : comRingType) (f : {rmorphism R -> R'}) (a b : R).
-
-Goal f ((a + b) ^+ 2) = f a ^+ 2 + f b ^+ 2 + 2%:R * f a * f b.
-Proof. ring. Qed.
-
-End RingMorphism.
-
-Section Additive.
-
-Variables (V' V : zmodType) (R : comRingType).
-Variables (g : {additive V' -> V}) (f : {additive V -> R}) (a : V') (b : V).
-
-Goal f (g a + b) ^+ 2 = f (g a) ^+ 2 + f b ^+ 2 + f (g (a *+ 2)) * f b.
-Proof. ring. Qed.
-
-End Additive.
-
-Section NumeralExamples.
-
-Lemma abstract_constants (R : comRingType): 200%:R * 30%:R = 6000%:R :> R.
+Goal 200%:R * 30%:R = 6000%:R :> R.
 Proof.
 Time ring. (* 0.186 secs *)
 Qed.
 
-Lemma int_constants : 200%:R * 30%:R = 6000%:R :> int.
+Goal 200%:R * 30%:R = 6000%:R :> int.
 Proof.
 Time ring. (* 0.343 secs *)
 Qed.
 
-Lemma rat_constants : 20%:R * 3%:R = 60%:R :> rat.
+Goal 20%:R * 3%:R = 60%:R :> rat.
 Proof.
 Time ring. (* 0.018 secs *)
 Qed.
 
-Lemma rat_constants_larger : 200%:R * 30%:R = 6000%:R :> rat.
+Goal 200%:R * 30%:R = 6000%:R :> rat.
 Proof.
 Time ring. (* 0.208 secs *)
 Qed.
@@ -103,13 +100,12 @@ End NumeralExamples.
 
 Section MoreVariables.
 
-Variables (q w e r t y u i o p a s d f g h j k l 
-: int).
+Variables (q w e r t y u i o p a s d f g h j k l : int).
 
 Lemma test_vars : 
-    q * w * e * r * t * y * u * i * o * p * a * s * d * f * g * h * j * k * l
- =  l * w * e * r * t * y * u * i * o * p * a * s * d * f * g * h * j * k * q.
-  Proof. Time ring. Qed. (* 0.049 secs *)
+  q * w * e * r * t * y * u * i * o * p * a * s * d * f * g * h * j * k * l =
+  l * w * e * r * t * y * u * i * o * p * a * s * d * f * g * h * j * k * q.
+Proof. Time ring. Qed. (* 0.049 secs *)
 
 End MoreVariables.
 
